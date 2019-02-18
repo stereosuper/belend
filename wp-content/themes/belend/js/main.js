69ef83cdb2e359ea442306422f36a8bf59a21cd0
Elisabeth Hamel <elisabeth@stereosuper.fr>
2019-02-18 15:58:31 +0100

    ?

--- wp-content/themes/belend/js/main.js
+++ wp-content/themes/belend/js/main.js
@@ -1288,9 +1288,9 @@ var throttle = function throttle(callback, delay) {
   !*** ./wp-content/themes/belend/src/scss/main.scss ***!
   \*****************************************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/lib/loader.js):\n\n@import \"pages/home\", \"pages/contact\";\n^\n      File to import not found or unreadable: pages/contact.\n      in /Users/elisabeth/Travail/Stereo/belend/wp-content/themes/belend/src/scss/main.scss (line 17, column 1)\n    at runLoaders (/Users/elisabeth/Travail/Stereo/belend/node_modules/webpack/lib/NormalModule.js:301:20)\n    at /Users/elisabeth/Travail/Stereo/belend/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/elisabeth/Travail/Stereo/belend/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Users/elisabeth/Travail/Stereo/belend/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.render [as callback] (/Users/elisabeth/Travail/Stereo/belend/node_modules/sass-loader/lib/loader.js:52:13)\n    at Object.done [as callback] (/Users/elisabeth/Travail/Stereo/belend/node_modules/neo-async/async.js:8077:18)\n    at options.error (/Users/elisabeth/Travail/Stereo/belend/node_modules/node-sass/lib/index.js:294:32)");
+// extracted by mini-css-extract-plugin
 
 /***/ })
 
