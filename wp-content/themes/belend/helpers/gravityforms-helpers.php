5d4a3814e16702759d14cd8fc6c4ca1c0ff3f9a2
James Prevot <jamesprevot@macbook-pro-de-james-goliath.home>
2019-04-18 15:10:01 +0200

    code: remove auto send of partial forms to Salesforce

--- wp-content/themes/belend/helpers/gravityforms-helpers.php
+++ wp-content/themes/belend/helpers/gravityforms-helpers.php
@@ -1,5 +1,5 @@
 <?php
-add_action("gform_partialentries_post_entry_saved", "belend_send_partial_entry", 10, 2);
+/*add_action("gform_partialentries_post_entry_saved", "belend_send_partial_entry", 10, 2);
 add_action("gform_partialentries_post_entry_updated", "belend_send_partial_entry", 10, 2);
 
 function belend_send_partial_entry($partial_entry, $form)
@@ -11,7 +11,7 @@ function belend_send_partial_entry($partial_entry, $form)
         $vxg_salesforce->push($partial_entry, $form);
     }
 
-}
+}*/
 
 
 /**
